a3188448963e37a3cd039adbffc2b99f462a73ee
DATBOI <alexsimons9999@gmail.com>
2018-06-23 19:16:51 -0500

    I can get a window, there needs to be only one tho

---
+++ src/main/java/com/chrisrm/idea/MonikasWritingTipOfTheDayManager.kt
@@ -0,0 +1,26 @@
+package com.chrisrm.idea
+
+import com.intellij.ide.util.TipDialog
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.openapi.wm.ex.WindowManagerEx
+
+class MonikasWritingTipOfTheDayManager : StartupActivity, DumbAware {
+
+    override fun runActivity(project: Project) {
+        if (ApplicationManager.getApplication().isUnitTestMode) return
+
+        println(WindowManagerEx.getInstanceEx().getFocusedComponent(project))
+
+        ToolWindowManager.getInstance(project).invokeLater {
+            if (!project.isDisposed)
+                ToolWindowManager.getInstance(project).invokeLater {
+                    if (!project.isDisposed)
+                        TipDialog.createForProject(project).show()
+                }
+        }
+    }
+}
